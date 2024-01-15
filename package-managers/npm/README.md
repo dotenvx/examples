504e68dcfc3725a42d38cf1d1d0ca0b8180a4449
Scott Motte <mot@mot.la>
2024-01-15 13:17:47 -0800

    add package-managers examples

---
+++ package-managers/npm/README.md
@@ -0,0 +1,6 @@
+# package-managers/npm
+
+[https://dotenvx.com/docs/package-managers/npm](https://dotenvx.com/docs/package-managers/npm)
+
+* [Express](./express)
+* [Remix](./remix)
---
+++ package-managers/npm/express/index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send(`Hello ${process.env.HELLO || ''}`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}`)
+})
